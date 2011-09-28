d009f0f5a7778b02929ecd54451c18fde7c9f580
Adrian Arias <aarias239@gmail.com>
2011-09-27 22:49:00 -0400

    removing node_modules from scm. Adding Cairo

--- controllers/blog.js
+++ controllers/blog.js
@@ -10,6 +10,21 @@ var Canvas = require('canvas');
 
 // GET /blog
 exports.index = function(req, res){
+  canvas = new Canvas(200,200),
+  ctx = canvas.getContext('2d');
+  
+  ctx.font = '30px Impact';
+  ctx.rotate(.1);
+  ctx.fillText("Awesome!", 50, 100);
+  
+  var te = ctx.measureText('Awesome!');
+  ctx.strokeStyle = 'rgba(0,0,0,0.5)';
+  ctx.beginPath();
+  ctx.lineTo(50, 102);
+  ctx.lineTo(50 + te.width, 102);
+  ctx.stroke();
+  console.log('<img src="' + canvas.toDataURL() + '" />');
+  debugger;
 
   articles = Articles.find({}, function(err, doc){
     if(err){
